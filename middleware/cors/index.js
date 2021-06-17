628af6bf45387a68b750f782f547c0f922a29845
Dro0ps <darnel.montero@gmail.com>
2021-06-17 12:24:28 -0400

    /

--- middleware/cors/index.js
+++ middleware/cors/index.js
@@ -1,7 +1,7 @@
 const cors = require ('cors');
 
 const corsOption = {
-    origin: ['http://localhost:3000', 'https://hardcore-newton-f6704f.netlify.app/*'],
+    origin: ['/*', 'https://hardcore-newton-f6704f.netlify.app/*'],
     optionsSuccessStatus: 200
 };
 
