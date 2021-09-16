489e8906982adb529ddfc0a0ff4fc3cb5ecefdba
Dro0ps <darnel.montero@gmail.com>
2021-09-16 16:56:44 -0300

    pedido con factura y fecha con hora

--- models/Pedido.js
+++ models/Pedido.js
@@ -65,7 +65,7 @@ const PedidoSchema = mongoose.Schema({
         type: String,
         required: false,
         trim: true,
-        default: "sin asignar"
+        
     },
     lugar_entrega: {
         type: String,
